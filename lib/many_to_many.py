5ad727ee9566a6b321c667cdc29e056271904577
dkiriinya <dkiriinya06sc@gmail.com>
2024-03-12 10:12:27 +0300

    many to many object relationship

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,64 @@
-class Author:
-    pass
-
-
 class Book:
-    pass
+    all_books = []
+
+    def __init__(self, title):
+        self.title = title
+        self.__class__.all_books.append(self)
+        
+    def contracts(self):
+        return [contract for contract in Contract.all_contracts if contract.book == self]
+    
+    def authors(self):
+        return [author for author in Author.all_authors]
+
+class Author:
+    all_authors = []
+
+    def __init__(self, name):
+        self.name = name
+        self.__class__.all_authors.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all_contracts if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        if not isinstance(book, Book):
+            raise Exception("Invalid book object.")
+        if not isinstance(date, str):
+            raise Exception("Invalid date format.")
+        if not isinstance(royalties, (int)):
+            raise Exception("Invalid royalties format.")
+
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Contract:
-    pass
+    all_contracts = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author) or not isinstance(book, Book):
+            raise Exception("Invalid author or book object.")
+        if not isinstance(date, str):
+            raise Exception("Invalid date format.")
+        if not isinstance(royalties, (int)) :
+            raise Exception("Invalid royalties format.")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        self.__class__.all_contracts.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all_contracts if contract.date == date]
+
+
+
